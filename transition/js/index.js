53e366bcbca5f72a168eed4cfced8a5b80ebc75a
qiantiansheng <qtiansheng@139.com>
2019-07-24 17:59:35 +0800

    简单的菜单动画效果

---
+++ transition/js/index.js
@@ -0,0 +1,10 @@
+
+function change() {
+    window.document.getElementById("text-id").style.color = "blue";
+    window.document.getElementById("line-id").style.width ="300px";
+}
+
+function outbutton() {
+    window.document.getElementById("text-id").style.color = "black";
+    window.document.getElementById("line-id").style.width = "0px";
+}
